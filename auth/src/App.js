6328f087ffe44ceb29389700745a24399918c343
Meet Vaghasiya <meet.vaghasiya@simformsolutions.com>
2024-10-17 12:36:50 +0530

    feat: correted signin file name

--- auth/src/App.js
+++ auth/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import {  Router, Route, Switch } from "react-router-dom";
 
 import { StylesProvider } from "@material-ui/core/styles";
-import SignIn from './components/SignIn';
+import SignIn from './components/Signin';
 import SignUp from './components/Signup';
 
 export default function App( { history , onSignIn} ) {
